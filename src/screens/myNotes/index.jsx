bdcfe470f800ee9e15e94fa5e314a2fd87e7c3a0
emreertugrl <emrertugrl@gmail.com>
2025-04-16 11:21:37 +0300

    button navigate to add note page and design completed

--- src/screens/myNotes/index.jsx
+++ src/screens/myNotes/index.jsx
@@ -1,12 +1,19 @@
 import {SafeAreaView, View} from 'react-native';
 import {screenStyle} from '../../styles/screenStyle';
 import Header from '../../components/router/header';
+import FloatActionButton from '../../components/uı/floatActionButton';
+import {ROUTES} from '../../utils/router';
+// import {useNavigation} from '@react-navigation/native';
 
-const MyNotes = () => {
+const MyNotes = ({navigation}) => {
+  // const navigation = useNavigation();
   return (
     <SafeAreaView style={screenStyle.container}>
       <View style={screenStyle.container}>
         <Header />
+        <FloatActionButton
+          onPress={() => navigation.navigate(ROUTES.AddNote)}
+        />
       </View>
     </SafeAreaView>
   );
